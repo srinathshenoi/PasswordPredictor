0907fc234b5e8c9f3f1dd046280fd980f70d97f5
Srinath Muralidharan <msrinath@prokarma.com>
2018-01-05 15:09:01 +0530

    Add project files.

---
+++ GenesisPredictor/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GenesisPredictor
+{
+    class Program
+    {
+        private static Random random = new Random();
+        static void Main(string[] args)
+        {
+            //Random random = new Random();
+            var target = "home";
+            var guess = GenerateRandom(target.Length);
+            Console.WriteLine("target - " + target);
+            Console.WriteLine("Guess - " + guess);
+            CheckFitness(guess, target);
+        }
+
+        private static void CheckFitness(string guess, string target)
+        {
+            //var fitness = Compare(guess, target);
+            var count = 0;
+            while (!guess.Equals(target))
+            {
+                count++;
+                var fitness = Compare(guess, target);
+                //Console.WriteLine(fitness.Count);
+                //Thread.Sleep(50);
+                guess = ModifyGuess(guess, fitness);
+                Console.WriteLine("Fitness - "+fitness.Count +" Count - " + count+ " New Guess - " + guess);
+            }
+            Console.WriteLine("\nAnswer - " + guess);
+        }
+
+        private static string ModifyGuess(string guess, List<int> fitness)
+        {
+            var newGuess = new StringBuilder();
+            var library = "qwertyuiopasdfghjklzxcvbnm";
+            //var random = new Random();
+            for (int guessIndex = 0; guessIndex < guess.Length; guessIndex++)
+            {
+                if (!fitness.Contains(guessIndex))
+                {
+                    var RandomIndex = random.Next(0, library.Length);
+                    newGuess.Insert(guessIndex,library[RandomIndex]);
+                }
+                else
+                {
+                    newGuess.Insert(guessIndex, guess[guessIndex]);
+                }
+            }
+            return newGuess.ToString();
+        }
+
+        private static List<int> Compare(string guess, string target)
+        {
+            var fitnessIndexes = new List<int>();
+            for (int guessCharIndex = 0; guessCharIndex < guess.Length; guessCharIndex++)
+            {
+                if (guess[guessCharIndex].Equals(target[guessCharIndex]))
+                {
+                    fitnessIndexes.Add(guessCharIndex);
+                }
+            }
+            return fitnessIndexes;
+        }
+
+        private static string GenerateRandom(int length)
+        {
+            var library = "qwertyuiopasdfghjklzxcvbnm";
+            var guess = new StringBuilder();
+            //var random = new Random();
+            while (length > guess.Length)
+            {
+                var guessIndex = random.Next(0, library.Length);
+                guess.Append(library[guessIndex]);
+            }
+            return guess.ToString();
+        }
+    }
+}
---
+++ PasswordPredictor/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordPredictor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            //Randomgeneration(input);
+            var output = Check(input);
+            Console.WriteLine(output);
+
+        }
+
+        //private static string Check(string input)
+        //{
+        //    var libraryStringSet = "asiowertyupdfghjklcvbnmxqz";
+        //    var random = new Random();
+        //    var num = 0;
+        //    var count = 0;
+        //    var outputString = new StringBuilder();
+        //    for (int indexOfInput = 0; indexOfInput < input.Length; indexOfInput++)
+        //    {
+        //        var list = new List<int>();
+        //        var inputchar = input[indexOfInput];
+        //        do
+        //        {
+        //            count++;
+        //            num = random.Next(1, 26);
+        //            Console.WriteLine(count);
+        //            if (inputchar.Equals(libraryStringSet[num]))
+        //            {
+        //                outputString.Append(libraryStringSet[num]);
+        //                break;
+        //            }
+
+        //        }
+        //        while (!list.Contains(num));
+        //        list.Add(num);
+        //    }
+        //    return outputString.ToString();
+
+        //}
+
+        private static string Check(string input)
+        {
+            var outputString = new StringBuilder();
+            var count = 0;
+            var libraryStringSet = "asiowertyupdfghjklcvbnmxqz";
+            var librarySpecialSet = " .!*@&#$%^+";
+            var librarySpecialSetLength = librarySpecialSet.Length;
+            var libraryStringsetLength = libraryStringSet.Length;
+
+            for (int indexOfInput = 0; indexOfInput < input.Length; indexOfInput++)
+            {
+                var inputchar = input[indexOfInput];
+                if (libraryStringSet.Contains(char.ToLower(inputchar)))
+                {
+                    for (int index = 0; index < libraryStringsetLength; index++)
+                    {
+                        count++;
+                        if (inputchar.Equals(libraryStringSet[index]))
+                        {
+                            outputString.Append(libraryStringSet[index]);
+                            break;
+                        }
+                        else if (inputchar.Equals(char.ToUpper(libraryStringSet[index])))
+                        {
+                            outputString.Append(char.ToUpper(libraryStringSet[index]));
+                            break;
+                        }
+                        Console.WriteLine(count);
+                    }
+                }
+                else
+                {
+                    for (int index = 0; index < librarySpecialSetLength; index++)
+                    {
+                        count++;
+                        if (inputchar.Equals(librarySpecialSet[index]))
+                        {
+                            outputString.Append(librarySpecialSet[index]);
+                            break;
+                        }
+                        Console.WriteLine(count);
+                    }
+                }
+            }
+            return outputString.ToString();
+        }
+
+
+    }
+}
